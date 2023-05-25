ded3f2ec00320bacdd88b47ca42cc5684b4262b8
Md.Talal Wasim <md.talalwasim@gmail.com>
2023-05-25 12:38:54 +0600

    @NotNull , @NotEmpty, @NotBlank is implemented for validation.

--- springrestapi/src/main/java/com/mdtalalwasim/springrestapi/entity/Employee.java
+++ springrestapi/src/main/java/com/mdtalalwasim/springrestapi/entity/Employee.java
@@ -12,6 +12,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,7 +30,9 @@ public class Employee {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull(message = "Employee name should not be null!")
+	//@NotNull(message = "Employee name should not be null!")
+	//@NotEmpty(message = "Employee name should not be null!")
+	@NotBlank(message = "Employee name should not be null!")
 	private String name;
 	
 	private Long age = 0L;
@@ -38,7 +42,9 @@ public class Employee {
 	@Email(message = "Please input valid email!")
 	private String email;
 	
-	@NotNull(message = "Employee Department should not be null!")
+	//@NotNull(message = "Employee Department should not be null!")
+	//@NotEmpty(message = "Employee Department should not be null!")
+	@NotBlank(message = "Employee Department should not be null!")
 	private String department;
 	
 	@CreationTimestamp
@@ -46,7 +52,7 @@ public class Employee {
 	private Date createdAt;
 	
 	@UpdateTimestamp
-	@Column(name = "updated_at")
+	@Column(name = "updated_at") 
 	private Date updatedAt;
 	
 	
